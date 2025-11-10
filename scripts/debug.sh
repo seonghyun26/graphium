36529bcc33a96b6ff2767fd1b1e02c2cdb404fdb
hyun <hyun26@kaist.ac.kr>
2025-11-10 06:59:36 +0000

    udp - debug script

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -1,25 +1,37 @@
 cd ../
 
 DEVICE=${1:-0}
-echo "Using device: $DEVICE"
-
-# TASK_LIST=('caco2_wang' 'lipophilicity_astrazeneca' 'solubility_aqsoldb' 'ppbr_az' 'vdss_lombardo' 'half_life_obach' 'clearance_microsome_az' 'clearance_hepatocyte_az' 'ld50_zhu')
-TASK_LIST=(cyp2d6_veith cyp3a4_veith cyp2c9_veith cyp2d6_substrate_carbonmangels cyp3a4_substrate_carbonmangels cyp2c9_substrate_carbonmangels herg ames dili)
+TASK_LIST=('caco2_wang')
+# TASK_LIST=(
+#     'caco2_wang' 'hia_hou' 'pgp_broccatelli' 'bioavailability_ma' 'lipophilicity_astrazeneca' 'solubility_aqsoldb' 
+#     'bbb_martins' 'ppbr_az' 'vdss_lombardo' 
+#     'cyp2d6_veith' 'cyp3a4_veith' 'cyp2c9_veith' 'cyp2c9_substrate_carbonmangels' 'cyp2d6_substrate_carbonmangels' 'cyp3a4_substrate_carbonmangels' 
+#     'half_life_obach' 'clearance_hepatocyte_az' 'clearance_microsome_az' 
+#     'ld50_zhu' 'herg' 'ames' 'dili' 
+# )
+HIDDEN_DIM_LIST=(696)
 
 for task in "${TASK_LIST[@]}"; do
-    echo $task
+    echo "Task: $task"
+    sleep 1
+
     CUDA_VISIBLE_DEVICES=$DEVICE graphium-train \
         model=gcn \
         accelerator=gpu \
         tasks=admet \
         ++constants.task=$task \
         ++datamodule.args.tdc_benchmark_names=$task \
-        ++architecture.gnn.hidden_dims=256 \
-        ++architecture.gnn.out_dim=256 \
-        ++architecture.gnn.depth=8 \
         ++constants.seed=0 \
-        ++constants.wandb.save_dir=null \
         ++constants.wandb.entity=eddy26 \
-        ++constants.wandb.tags="['debug']" \
-        ++constants.wandb.project=graphium
+        ++constants.wandb.save_dir=null \
+        ++constants.wandb.project=graphium \
+        ++constants.wandb.tags="['gcn', 'scratch']" \
+        ++architecture.pre_nn.out_dim=${hidden_dim} \
+        ++architecture.gnn.depth=16 \
+        ++architecture.gnn.in_dim=${hidden_dim} \
+        ++architecture.gnn.out_dim=${hidden_dim} \
+        ++architecture.gnn.hidden_dims=${hidden_dim} \
+        ++architecture.graph_output_nn.graph.hidden_dims=${hidden_dim} \
+        ++architecture.graph_output_nn.graph.out_dim=${hidden_dim} \
+        ++architecture.task_heads.${task}.hidden_dims=${hidden_dim} 
 done
