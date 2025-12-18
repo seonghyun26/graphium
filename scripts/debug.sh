464fc0fe229ef96e089e24417fe4e9a5d2e29dd0
hyun <hyun26@kaist.ac.kr>
2025-12-18 09:48:39 +0000

    upd - largemix and gpspp

--- scripts/debug.sh
+++ scripts/debug.sh
@@ -12,7 +12,7 @@ TASK_LIST=('hia_hou')
 HIDDEN_DIM=512
 
 
-# ADMET scratch
+ADMET scratch
 for task in "${TASK_LIST[@]}"; do
     echo "Training $task"
     sleep 1
@@ -39,14 +39,14 @@ for task in "${TASK_LIST[@]}"; do
         ++architecture.task_heads.${task}.hidden_dims=$HIDDEN_DIM 
 done
 
-# Pretraining
-echo "Pretraining on toy dataset"
+# Pretraining largemix
+echo "Pretraining on large dataset"
 CUDA_VISIBLE_DEVICES=$DEVICE graphium-train \
     model=gcn \
     accelerator=gpu \
-    tasks=toymix \
-    training=toymix \
-    architecture=toymix \
+    tasks=largemix \
+    training=largemix \
+    architecture=largemix \
     ++constants.seed=0 \
     ++constants.wandb.entity=eddy26 \
     ++constants.wandb.save_dir=null \
@@ -68,4 +68,19 @@ CUDA_VISIBLE_DEVICES=$DEVICE graphium-train \
     ++datamodule.args.task_specific_args.tox21.df_path=./data/graphium/neurips2023/small-dataset/Tox21-7k-12-labels.csv \
     ++datamodule.args.task_specific_args.tox21.splits_path=./data/graphium/neurips2023/small-dataset/Tox21_random_splits.pt \
     ++datamodule.args.task_specific_args.zinc.df_path=./data/graphium/neurips2023/small-dataset/ZINC12k.csv \
-    ++datamodule.args.task_specific_args.zinc.splits_path=./data/graphium/neurips2023/small-dataset/ZINC12k_random_splits.pt
+    ++datamodule.args.task_specific_args.zinc.splits_path=./data/graphium/neurips2023/small-dataset/ZINC12k_random_splits.pt \
+    ++datamodule.args.task_specific_args.qm9.df_path=./data/graphium/neurips2023/small-dataset/qm9.csv \
+    ++datamodule.args.task_specific_args.qm9.splits_path=./data/graphium/neurips2023/small-dataset/qm9_random_splits.pt \
+    ++datamodule.args.task_specific_args.tox21.df_path=./data/graphium/neurips2023/small-dataset/Tox21-7k-12-labels.csv \
+    ++datamodule.args.task_specific_args.tox21.splits_path=./data/graphium/neurips2023/small-dataset/Tox21_random_splits.pt \
+    ++datamodule.args.task_specific_args.zinc.df_path=./data/graphium/neurips2023/small-dataset/ZINC12k.csv \
+    ++datamodule.args.task_specific_args.zinc.splits_path=./data/graphium/neurips2023/small-dataset/ZINC12k_random_splits.pt \
+    ++datamodule.args.task_specific_args.pcqm4m_g25_n4.df_path=./data/graphium/neurips2023/large-dataset/PCQM4M_G25_N4.parquet \
+    ++datamodule.args.task_specific_args.pcqm4m_g25_n4.splits_path=./data/graphium/neurips2023/large-dataset/pcqm4m_g25_n4_random_splits.pt \
+    ++datamodule.args.task_specific_args.pcba_1328.df_path=./data/graphium/neurips2023/large-dataset/PCBA_1328_1564k.parquet \
+    ++datamodule.args.task_specific_args.pcba_1328.splits_path=./data/graphium/neurips2023/large-dataset/pcba_1328_random_splits.pt \
+    ++datamodule.args.task_specific_args.l1000_vcap.df_path=./data/graphium/neurips2023/large-dataset/LINCS_L1000_VCAP_0-2_th2.csv \
+    ++datamodule.args.task_specific_args.l1000_vcap.splits_path=./data/graphium/neurips2023/large-dataset/l1000_vcap_random_splits.pt \
+    ++datamodule.args.task_specific_args.l1000_mcf7.df_path=./data/graphium/neurips2023/large-dataset/LINCS_L1000_MCF7_0-2_th2.csv \
+    ++datamodule.args.task_specific_args.l1000_mcf7.splits_path=./data/graphium/neurips2023/large-dataset/l1000_mcf7_random_splits.pt \
+
