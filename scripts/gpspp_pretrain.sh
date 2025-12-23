8a9a5269e88023d3d065a14c80ccc5276010a5c2
hyun <hyun26@kaist.ac.kr>
2025-12-23 05:07:09 +0000

    upd - gpspp training configs and scripts

--- scripts/gpspp_pretrain.sh
+++ scripts/gpspp_pretrain.sh
@@ -1,6 +1,7 @@
 cd ../
 
 DEVICE=${1:-'0'}
+DIM=2048
 
 CUDA_VISIBLE_DEVICES=$DEVICE graphium-train \
     model=gpspp \
@@ -14,11 +15,11 @@ CUDA_VISIBLE_DEVICES=$DEVICE graphium-train \
     ++constants.wandb.project=graphium \
     ++constants.wandb.tags="['gpspp','pretrain','largemix']" \
     ++constants.norm=layer_norm \
-    ++datamodule.args.batch_size_training=300 \
-    ++architecture.pre_nn.out_dim=320 \
-    ++architecture.pre_nn.hidden_dims=320 \
-    ++architecture.gnn.layer_kwargs.mpnn_kwargs.in_dim=320 \
-    ++architecture.gnn.layer_kwargs.mpnn_kwargs.out_dim=320 \
-    ++architecture.gnn.in_dim=320 \
-    ++architecture.gnn.hidden_dims=320 \
-    ++architecture.gnn.out_dim=320
+    ++datamodule.args.batch_size_training=400 \
+    ++architecture.pre_nn.out_dim=$DIM \
+    ++architecture.pre_nn.hidden_dims=$DIM \
+    ++architecture.gnn.layer_kwargs.mpnn_kwargs.in_dim=$DIM \
+    ++architecture.gnn.layer_kwargs.mpnn_kwargs.out_dim=$DIM \
+    ++architecture.gnn.in_dim=$DIM \
+    ++architecture.gnn.hidden_dims=$DIM \
+    ++architecture.gnn.out_dim=$DIM
